c1d516765b873f097e72ff15a8a9fea4f5d14182
wojaczek.piotr <wojaczek-piotr@wp.pl>
2019-10-22 17:41:02 +0200

    first commit

---
+++ snippets/serializers.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+#
+#     def create(self, validated_data):
+#         return Snippet.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_date):
+#         instance.title = validated_date.get('title', instance.title)
+#         instance.code = validated_date.get('code', instance.code)
+#         instance.linenos = validated_date.get('linenos', instance.linenos)
+#         instance.language = validated_date.get('language', instance.language)
+#         instance.style = validated_date.get('style', instance.sty)
+#         instance.save()
+#         return instance
